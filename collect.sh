358b7c1fc49ec925d4b5f446f7487ebc5e96ced4
jiriklepl <jiriklepl@seznam.cz>
2021-09-04 21:37:35 +0000

    fixed collect.sh, added ./data to .gitignore

--- collect.sh
+++ collect.sh
@@ -6,6 +6,6 @@ for strategy in $(seq 4); do
 	DIR_NAME="data/$strategy"
 	mkdir -p "$DIR_NAME"
 	OUTPUT_CSV="$DIR_NAME/$CPU_COUNT.csv"
-	echo "running: numactl --physcpubind=$(./cpuset.sh "$CPU_COUNT" "$strategy" | awk 'BEGIN { i = 0 } { if (i++ > 0) printf "," $1; else printf $1 } END { print "" }') ./run_script.sh"
-	numactl --physcpubind=$(./cpuset.sh "$CPU_COUNT" "$strategy" | awk 'BEGIN { i = 0 } { if (i++ > 0) printf "," $1; else printf $1 } END { print "" }') ./run_script.sh
+	echo "running: numactl --physcpubind=$(./cpuset.sh "$CPU_COUNT" "$strategy" | awk 'BEGIN { i = 0 } { if (i++ > 0) printf "," $1; else printf $1 } END { print "" }') ./run-script.sh \"$OUTPUT_CSV\""
+	numactl --physcpubind=$(./cpuset.sh "$CPU_COUNT" "$strategy" | awk 'BEGIN { i = 0 } { if (i++ > 0) printf "," $1; else printf $1 } END { print "" }') ./run-script.sh "$OUTPUT_CSV"
 done
