a51cbfc1e8a3a5497097dc00f0e2c554ad978c70
jiriklepl <jiriklepl@seznam.cz>
2021-09-07 02:47:57 +0000

    added a TODO; style

--- script.R
+++ script.R
@@ -2,6 +2,7 @@
 
 library('tidyverse')
 
+# TODO: add cpu set strategy
 data <- list.files(path="data", recursive=TRUE, pattern="*.csv", full.names=TRUE) %>% map((function(x) (read_csv(x, show_col_types=FALSE) %>% mutate(cores=as.integer(substr(x, 8, str_length(x) - 4)))))) %>% bind_rows
 
-data %>% group_by(benchmark, cores) %>% summarize(duration = mean(duration_ns), std_dev = sd(duration_ns))
+data %>% group_by(benchmark, cores) %>% summarize(duration=mean(duration_ns), std_dev=sd(duration_ns))
