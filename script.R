4d8bea41804f3106089069f9c48c832125c601ad
jiriklepl <jiriklepl@seznam.cz>
2021-09-07 02:36:14 +0000

    added the initial R script

---
+++ script.R
@@ -0,0 +1,7 @@
+#!/usr/bin/env Rscript
+
+library('tidyverse')
+
+data <- list.files(path="data", recursive=TRUE, pattern="*.csv", full.names=TRUE) %>% map((function(x) (read_csv(x, show_col_types=FALSE) %>% mutate(cores=as.integer(substr(x, 8, str_length(x) - 4)))))) %>% bind_rows
+
+data %>% group_by(benchmark, cores) %>% summarize(duration = mean(duration_ns), std_dev = sd(duration_ns))
