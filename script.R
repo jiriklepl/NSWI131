3bcf07a8c2da09072a57356789fd532d4ae6d0c7
jiriklepl <jiriklepl@seznam.cz>
2021-09-11 04:57:35 +0200

    included the small dataset

--- script.R
+++ script.R
@@ -10,6 +10,7 @@ get_data <- function(data_dir) (
 
 data1 <- get_data("data")
 data2 <- get_data("data2")
+data3 <- get_data("data3")
 
 data <- bind_rows(data1, data2)
 
@@ -17,5 +18,8 @@ data <- bind_rows(data1, data2)
 # data2 <- data2 %>% group_by(benchmark, strategy, cores) %>% summarize(dur=mean(duration_ns), dur_sd=sd(duration_ns), dur_sd_rel=dur_sd/dur, heap=mean(heap_size_after-heap_size_before), heap_sd=sd(heap_size_after-heap_size_before), heap_sd_rel=heap_sd/heap)
 # data <- data %>% group_by(benchmark, strategy, cores) %>% summarize(dur=mean(duration_ns), dur_sd=sd(duration_ns), dur_sd_rel=dur_sd/dur, heap=mean(heap_size_after-heap_size_before), heap_sd=sd(heap_size_after-heap_size_before), heap_sd_rel=heap_sd/heap)
 
-(data %>% group_by(benchmark, strategy) %>% do(plots=ggplot(data=.) + aes(x=factor(cores), y=duration_ns) + geom_violin(trim=TRUE, draw_quantiles=c(.25,.5,.75)) + stat_summary(data = . %>% group_by(cores) %>% summarize(dur=mean(duration_ns)), aes(x=factor(cores), y=dur, group=1, color=1), fun.y=sum, geom="line") + ggtitle(unique(paste(.$benchmark, .$strategy, sep="-"))) + theme(legend.position = "none")))$plots
-(data %>% group_by(benchmark, strategy) %>% do(plots=ggplot(data=.) + aes(x=factor(cores), y=heap_size_after-heap_size_before) + geom_violin(trim=TRUE, draw_quantiles=c(.25,.5,.75)) + stat_summary(data = . %>% group_by(cores) %>% summarize(dur=mean(heap_size_after-heap_size_before)), aes(x=factor(cores), y=dur, group=1, color=1), fun.y=sum, geom="line") + ggtitle(unique(paste(.$benchmark, .$strategy, sep="-"))) + theme(legend.position = "none")))$plots
+(data %>% group_by(benchmark, strategy) %>% do(plots=ggplot(data=.) + aes(x=factor(cores), y=duration_ns) + geom_violin(trim=TRUE, draw_quantiles=c(.25,.5,.75)) + stat_summary(data = . %>% group_by(cores) %>% summarize(dur=mean(duration_ns)), aes(x=factor(cores), y=dur, group=1, color=1), fun.y=sum, geom="line") + ggtitle(unique(paste(.$benchmark, .$strategy, "duration", sep="-"))) + theme(legend.position = "none")))$plots
+(data %>% group_by(benchmark, strategy) %>% do(plots=ggplot(data=.) + aes(x=factor(cores), y=heap_size_after-heap_size_before) + geom_violin(trim=TRUE, draw_quantiles=c(.25,.5,.75)) + stat_summary(data = . %>% group_by(cores) %>% summarize(dur=mean(heap_size_after-heap_size_before)), aes(x=factor(cores), y=dur, group=1, color=1), fun.y=sum, geom="line") + ggtitle(unique(paste(.$benchmark, .$strategy, "heap_usage", sep="-"))) + theme(legend.position = "none")))$plots
+
+(data3 %>% group_by(benchmark, strategy) %>% do(plots=ggplot(data=.) + aes(x=factor(cores), y=duration_ns) + geom_violin(trim=TRUE, draw_quantiles=c(.25,.5,.75)) + stat_summary(data = . %>% group_by(cores) %>% summarize(dur=mean(duration_ns)), aes(x=factor(cores), y=dur, group=1, color=1), fun.y=sum, geom="line") + ggtitle(unique(paste(.$benchmark, .$strategy, "duration", "small", sep="-"))) + theme(legend.position = "none")))$plots
+(data3 %>% group_by(benchmark, strategy) %>% do(plots=ggplot(data=.) + aes(x=factor(cores), y=heap_size_after-heap_size_before) + geom_violin(trim=TRUE, draw_quantiles=c(.25,.5,.75)) + stat_summary(data = . %>% group_by(cores) %>% summarize(dur=mean(heap_size_after-heap_size_before)), aes(x=factor(cores), y=dur, group=1, color=1), fun.y=sum, geom="line") + ggtitle(unique(paste(.$benchmark, .$strategy, "heap_usage", "small", sep="-"))) + theme(legend.position = "none")))$plots
